7490f8ddb0994cd67e23ff7fd1cbaeea48ff80cc
Emídio <emidioaranha@hotmail.com>
2020-08-19 00:30:02 -0400

    Atualizando os códigos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+//import Nav from './components/nav'
 //import logo from './logo.svg';
-import './App.css';
 
 //<img className="img-react" alt="img-react" src="https://media.bitdegree.org/storage/media/images/2018/10/The-Most-Essential-React-Interview-Questions.jpg"/>
 //Sem usar o modo classe. também funciona.
@@ -62,9 +62,12 @@ class App extends React.Component{
    */ 
 
   insercao_dados = (evento) =>{
+
+    const dados = evento.target.value
+    const campos = evento.target.name
     
     this.setState({
-      nome: evento.target.value
+      [campos]: dados
     })
   }
 
@@ -80,17 +83,30 @@ class App extends React.Component{
 
   render(){
     return(
-      <div>
+      <div id="img-fundo">
         <header className="App-header">
           <div>
-            <h1>Seja bem vindo ao iniciando meus estudos com react!</h1>
+            <h1 className="custom-h1">Seja bem vindo ao SpaceStation!</h1>
           </div>
         </header>
-        <div id="div-info">
-          <label for="nome">Nome:</label>
-          <input id="nome" type="text" value={this.state.nome} onChange={this.insercao_dados} />
+        <div id="div-info1">
+          <label id="label1" for="nome">Nome: </label>
+          <input id="nome" type="text" value={this.state.nome} name="nome" onChange={this.insercao_dados} />
+          <label id="label1" for="data">DdeN: </label>
+          <input id="data" type="date" value={this.state.data} name="data" onChange={this.insercao_dados} />
+          <br/>
           {this.criaComboBox()}
-          <h1>Olá {this.state.nome}</h1>
+          <h1 className="custom-h1">Olá {this.state.nome} com data de nascimento em ({this.state.data}).</h1>
+        </div>
+        <div id="div-info2">
+          <p>
+            O que é o Lorem Ipsum? <br/>
+            O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.
+            <br/><br/>
+            Porque é que o usamos?<br/>
+            É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de "Conteúdo aqui, conteúdo aqui", tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por "lorem ipsum" irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor).
+            
+          </p>
         </div>
       <footer>
       </footer>
